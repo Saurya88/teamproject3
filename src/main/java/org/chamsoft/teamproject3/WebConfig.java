5f3cd8611896e93250c04bb5283b195efc42c03a
Saurya88 <tatinenisaurya@gmail.com>
2025-12-02 23:44:48 -0500

    WebConfig

--- src/main/java/org/chamsoft/teamproject3/WebConfig.java
+++ src/main/java/org/chamsoft/teamproject3/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://capable-belekoy-cbe79c.netlify.app/")
 // your React dev URL(s)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
